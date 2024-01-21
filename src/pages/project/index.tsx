4562205298364ed154aece575e5a9476e900e150
jiatianzhi <jiatianzhi@bjtu.edu.cn>
2024-01-21 23:59:18 +0800

    9 commit

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -17,7 +17,7 @@ const TITLE = translate({
 })
 const DESCRIPTION = translate({
   id: 'theme.project.description',
-  message: '学而无用，不如学而用之。这里是我在技术领域中努力实践和应用的最佳证明。',
+  message: 'Here is the best proof of my hard practice and application in the field of technology.',
 })
 
 // const GITHUB_URL = 'https://github.com/kuizuo'
