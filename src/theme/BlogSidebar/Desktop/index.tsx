4010f510ed17b1d1af7b2f735cd88ba400d95e75
jiatianzhi <jiatianzhi@bjtu.edu.cn>
2024-04-06 23:36:32 +0800

    37

--- src/theme/BlogSidebar/Desktop/index.tsx
+++ src/theme/BlogSidebar/Desktop/index.tsx
@@ -37,7 +37,7 @@ export default function BlogSidebarDesktop({ sidebar }: Props): JSX.Element {
           </div>
         )}
 
-        <Link href="/blogs" className={clsx(styles.sidebarItemTitle, 'margin-bottom--sm')}>
+        <Link href="/blog" className={clsx(styles.sidebarItemTitle, 'margin-bottom--sm')}>
           {sidebar.title}
         </Link>
         <ul className={clsx(styles.sidebarItemList, 'clean-list')}>
